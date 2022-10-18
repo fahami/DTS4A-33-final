84e197277eb35d9191152865a1a39a15c3b9ace9
Fahmi <fahmi@ekrut.com>
2022-10-18 21:59:19 +0700

    init movie project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@emotion/react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import MovieList from "./containers/MovieList";
+import theme from "./themes/theme";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<div className="App">
+				<Navbar></Navbar>
+				<Routes>
+					<Route path="/" element={<MovieList />} />
+				</Routes>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import { MovieFilter } from "@mui/icons-material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+	return (
+		<Box sx={{ display: "flex" }}>
+			<AppBar>
+				<Toolbar>
+					<MovieFilter sx={{ display: "flex", mr: 1 }} />
+					<Typography
+						variant="h6"
+						sx={{
+							flexGrow: 1,
+							display: "block",
+							fontFamily: "monospace",
+							fontWeight: 700,
+							letterSpacing: ".3rem",
+						}}
+					>
+						<Link
+							style={{ color: "inherit", textDecoration: "inherit" }}
+							to="/"
+						>
+							NONTON
+						</Link>
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
+};
+
+export default Navbar;
